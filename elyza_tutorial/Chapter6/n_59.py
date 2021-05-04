38f6fbda8ffd373f2cf3190f4c61aa49066783b9
tsomeya <tsomeya@student.42tokyo.jp>
2021-05-04 23:46:11 +0900

    lightgbm added

--- elyza_tutorial/Chapter6/n_59.py
+++ elyza_tutorial/Chapter6/n_59.py
@@ -1,7 +1,8 @@
 import scipy.stats
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import accuracy_score
+import optuna.integration.lightgbm as lgb
 import pandas as pd
 import pickle
 
@@ -71,6 +72,41 @@ C_list=[1e+2, 1e+1, 1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5]):
 	print(f"Accuracy on test_data: {test_acc}")
 
 
+def LightGBM_tuning(train_X, train_y, val_X, val_y, test_X, test_y):
+	"""異なる正則化パラメータでLightGBMを学習し検証データの正解率が最も高くなるモデルを見つけ、
+	その後評価データでの正解率を出力する
+	Args:
+		train_X 		：データ
+		train_y 		：データ
+		val_X 			：データ
+		val_y 			：データ
+		test_X 			：データ
+		test_y 			：データ
+	"""
+	train_data = lgb.Dataset(train_X, train_y)
+	val_data = lgb.Dataset(val_X, val_y, reference=train_data)
+	params = {
+		"objective": "multiclass",
+		"boosting_type": "gbdt",
+		"metric": "multi_logloss",
+		"num_class": "4",
+	}
+	# tuning
+	best_params, history = dict(), list()
+	model = lgb.train(params, train_data, valid_sets=val_data, verbose_eval=-1, best_params=best_params,tuning_history=history)
+
+	# accuracy on test_data
+	# best_model = lgb.train(best_params, train_data, valid_sets=val_data, verbose_eval=-1, best_params=best_params,tuning_history=history)
+	# test_acc = best_model.score(test_X, test_y)
+	y_pred = model.predict(test_X)
+	test_acc = accuracy_score(test_y, y_pred)
+	with open("lgb_best.pickle", mode="wb") as f:
+		pickle.dump(best_model, f)
+	print(f"Best params: {best_params}")
+	print(f"Accuracy on test_data: {test_acc}")
+
+
+
 if __name__ == "__main__":
 	# data loading...
 	train_X = pd.read_csv("train.feature.txt", sep="\t")
@@ -84,5 +120,7 @@ if __name__ == "__main__":
 	LR_tuning(train_X, train_y, val_X, val_y, test_X, test_y)
 	print("----LinearSVC----")
 	LinearSVC_tuning(train_X, train_y, val_X, val_y, test_X, test_y)
+	# print("---LightGBM---")
+	# LightGBM_tuning(train_X, train_y, val_X, val_y, test_X, test_y)
 
 
