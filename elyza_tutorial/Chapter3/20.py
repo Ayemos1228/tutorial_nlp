e2a751a0e55cc91b30504cbf4126d10037e88608
吉田遼 <yoshiryo0617@gmail.com>
2021-05-06 13:46:59 +0900

    Add comment

--- elyza_tutorial/Chapter3/20.py
+++ elyza_tutorial/Chapter3/20.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import re
+import re # いらない
 
 data = pd.read_json("jawiki-country.json",lines = True)
 text_uk = data[data["title"] =="イギリス"]["text"].iloc[0]
--- elyza_tutorial/Chapter3/22.py
+++ elyza_tutorial/Chapter3/22.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import re
 
-data = pd.read_json("jawiki-country.json",lines = True)
-text_uk = data[data["title"] =="イギリス"]["text"].iloc[0]
-print(re.findall(r"^.*\[\[Category:(\w+)",text_uk,flags=re.MULTILINE)
+data = pd.read_json("jawiki-country.json", lines=True)
+text_uk = data[data["title"] == "イギリス"]["text"].iloc[0]
+print(
+    re.findall(r"^.*\[\[Category:(\w+)", text_uk, flags=re.MULTILINE)
+)  # printが閉じられてないのでエラーになる
+# どうでもいいけど"欧州連合加盟国|元"の"|元"はそのまま取っておきたい感はある
